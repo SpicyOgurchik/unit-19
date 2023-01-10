574e2de293220da325e10a6181a5b1ba9d9225e6
SpicyOgurchik <107600965+SpicyOgurchik@users.noreply.github.com>
2023-01-09 21:26:57 -0500

    Add files via upload

---
+++ unit19.5/flask-boggle/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, jsonify, session
+from boggle import Boggle
+
+boggle_game = Boggle()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    """show board"""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+    """word is in the dictionary or not"""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """update nplays and high score"""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
